c8397b6b7eaccdb85712fc25e94e25fa10944aeb
sharperM <mshaopei@gmail.com>
2017-03-16 15:54:19 +0800

    整理代码

--- DllViewer/DllViewer.cpp
+++ DllViewer/DllViewer.cpp
@@ -39,14 +39,20 @@ bool CmpExeDlls(const ExeDlls& v1, const ExeDlls& v2)
 
 std::map<std::wstring,std::vector<std::string>> fun( const TCHAR* pszPath );
 std::string WChar2Ansi(LPCWSTR pwszSrc);
+
+//参数二 为要查找的目录路径
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::map<std::wstring,std::vector<std::string>> exeDlls;
 	std::map<std::string,int> allDlls;
-	std::set<std::string> allExes;
+
 	if (argc >1)
 	{
 		exeDlls = fun( argv[1] );
+	}
+	else
+	{
+		printf("参数太少了");
 	}
 	for ( std::map<std::wstring,std::vector<std::string>>::iterator it = exeDlls.begin(),itend = exeDlls.end();
 		it != itend; ++it)
@@ -55,9 +61,9 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			allDlls.insert(std::pair<std::string,int>(it->second[i],0));
 		}
-		allExes.insert( WChar2Ansi(it->first.c_str()) );
 	}
-	std::vector<bool> talbe;
+	
+	std::vector<bool> isDllUsed; //记录exe用到了哪些dll和没用到那些dll
 	for (std::map<std::string,int>::iterator i = allDlls.begin(),ic = allDlls.end();i!=ic;++i )
 	{
 		for (std::map<std::wstring,std::vector<std::string>>::iterator j = exeDlls.begin(), jc = exeDlls.end();j!=jc;++j)
@@ -67,183 +73,65 @@ int _tmain(int argc, _TCHAR* argv[])
 			{
 				if (j->second[jj]==(i->first))
 				{
-					talbe.push_back(true);
+					isDllUsed.push_back(true);
 					break;	
 				}
 			}
 			if (jj == jjc)
 			{
-				talbe.push_back(false);
+				isDllUsed.push_back(false);
 			}
 		}
 	}
-	std::string outppp;
-	std::map<std::string,int>::iterator itDll = allDlls.begin();
-	std::set<std::string>::iterator itExe = allExes.begin();
-	for ( int i = 0, ic = allDlls.size()+1; i<ic; ++i )
-	{
-		int line= 0;
-		for ( int j = 0, jc = exeDlls.size()+1; j < jc; ++j )
-		{
-			if ( i == 0 &&j>0)
-			{
-				
-				outppp += ",";
-				outppp += (*itExe++);
-			}
-			else if  (j == 0&&i > 0)
-			{
-				outppp += (itDll->first);
-				
-			}
-			else if (i>0&&j>0)
-			{
-				outppp+=",";
-				if (talbe[(j-1)+(jc-1)*(i-1)] == true)
-				{
-					outppp+="1";
-					++line;
-				}
-				else
-				{
-					outppp+="";
-				}
-			}
-		}
-		if ( i > 0 &&itDll!=allDlls.end())
-		{
-			itDll->second = line;
-			itDll++;
-		}
-		outppp+="\r\n";
-	}
-	std::vector<std::string> sortdlls;
-	std::vector<std::string> sorrExes;
-	for (std::map<std::string,int>::iterator it = allDlls.begin(),itend = allDlls.end();
-		it!=itend; ++it)
-	{
-		sortdlls.push_back(it->first);
-	}
-	//void insertSort(Type* arr,long len)/*InsertSort algorithm*/
-	{
-//		void bubbleSort(int arr[],int n)
+	//构造 csv
+	std::string strCsvContent=",";
+
+	int nIndexDll = 0, nIndexExe = 0;
 	
-		int i,j;
-		std::string t;
-		for(i=0;i<sortdlls.size()-1;i++)
+	for (std::map<std::wstring,std::vector<std::string>>::iterator iterED = exeDlls.begin(), iterEDend = exeDlls.end();iterED!=iterEDend;++iterED)
+	{//第一行
+		strCsvContent += WChar2Ansi(iterED->first.c_str());
+		strCsvContent += ",";
+	}
+	strCsvContent += "\r\n";
+	for ( std::map<std::string,int>::iterator iterDll =  allDlls.begin(), iterDllend =  allDlls.end();iterDll!=iterDllend;iterDll++ )
+	{
+		int dllBeUseCount= 0;
+		nIndexExe = 0;
+		for (std::map<std::wstring,std::vector<std::string>>::iterator iterED = exeDlls.begin(), iterEDend = exeDlls.end();iterED!=iterEDend;++iterED)
 		{
-			for(j=0;j<sortdlls.size()-i-1;j++)
+			if  ( iterED == exeDlls.begin() )
 			{
-				if(allDlls[sortdlls[j+1]]>allDlls[sortdlls[j]])
-				{
-					t=sortdlls[j+1];
-					sortdlls[j+1]=sortdlls[j];
-					sortdlls[j]=t;
-				}
+				//第一列
+				strCsvContent += iterDll->first;
+				strCsvContent += ",";
 			}
-		}
-	}
-	for (int i = 0,c = sortdlls.size(); i<c; ++i)
-	{
-		outppp+=sortdlls[i]+std::string(",");
-	}
-	//for (int i = 0, c = sortdlls.size(); i<c; ++i)
-	//{
-	//	for (std::map<std::wstring,std::vector<std::string>>::iterator j = exeDlls.begin() ;j!=exeDlls.end();++j)
-	//	{
-	//		int  ii = 0,cc = j->second.size();
-	//		for (; ii<cc; ++ii)
-	//		{
-	//			if (j->second[ii] == sortdlls[i] )
-	//			{
-	//				break;
-	//			}
-	//		}
-	//		if (ii == cc)
-	//		{
-	//			sorrExes.push_back(WChar2Ansi(j->first.c_str()));
-	//			exeDlls.erase(j);
-	//			break;
-	//		}
-	//	}
-	//}
-	{
-	/*	for (;sorrExes.size()<allExes.size() ;)
-		{
-			int ii = 0;
-			for (std::map<std::wstring,std::vector<std::string>>::iterator j = exeDlls.begin() ;j!=exeDlls.end();++j)
+			if (isDllUsed[(nIndexExe)+(exeDlls.size())*(nIndexDll)] == true)
 			{
-				int  i = 0,c = j->second.size();
-				for (; i<c; ++i)
-				{
-					if (j->second[i] == sortdlls[1] )
-					{
-						break;
-					}
-				}
-				if (i == c)
-				{
-					sorrExes.push_back(WChar2Ansi(j->first.c_str()));
-					exeDlls.erase(j);
-					break;
-				}
-				sortdlls.erase(sortdlls.begin());
+					strCsvContent+="1";
+					++dllBeUseCount;
 			}
+			strCsvContent += ",";
+			nIndexExe++;
 		}
-		*/
+		nIndexDll++;
+
+		iterDll->second = dllBeUseCount;
+		strCsvContent+="\r\n";
 	}
 
-	std::vector<ExeDlls> sortedExeDlls;
-	for (auto iter = exeDlls.begin(), iend = exeDlls.end();
-		iter!=iend; ++iter)
-	{
-		ExeDlls ed;
-		ed.exe = iter->first;
-		for (auto iter2 = allDlls.begin(), iend2 = allDlls.end();
-			iter2!=iend2; ++iter2)
-		{
-			ed.dlls[iter2->first] = false;
-		}
-		for (auto iter3 = iter->second.begin(), iend3 = iter->second.end();
-			iter3!=iend3; ++iter3)
-		{
-			ed.dlls[*iter3] = true;
-		}
-		sortedExeDlls.push_back(ed);
-	}
-
-	std::stable_sort(sortedExeDlls.begin(), sortedExeDlls.end(), &CmpExeDlls);
-
-	const char * filename = "C:\\Documents and Settings\\Administrator\\桌面\\新建 文本文档.csv";
+	const char * filename = ".\\exeDependDllList.csv";
 	std::ofstream fs(filename,std::ios::binary|std::ios::out);
-// 	std::ofstream fs("依赖的dll.csv",std::ios::binary);
-	//fs.write(outppp.c_str(),outppp.size());
-	// exe行
-	for (auto iter = sortedExeDlls.begin(), iend = sortedExeDlls.end();
-		iter!=iend; ++iter)
-	{
-		fs << "," << WChar2Ansi(iter->exe.c_str());
-	}
-	fs << "\r\n";
-	// 各DLL行
-	for (auto iter1 = sortedExeDlls.begin()->dlls.begin(), iend1 = sortedExeDlls.begin()->dlls.end();
-		iter1!=iend1; ++iter1)
-	{
-		fs << iter1->first << ",";
-		for (auto iter2 = sortedExeDlls.begin(), iend2 = sortedExeDlls.end();
-			iter2!=iend2; ++iter2)
-		{
-			fs << (iter2->dlls[iter1->first]?"1":"") << ",";
-		}
-		fs << "\r\n";
-	}
+	fs<<strCsvContent;
+
 	fs.flush();
 	fs.close();
 	ShellExecuteA(NULL, "open", filename, NULL, NULL, SW_SHOW);
-	//system("pause");
 	return 0;
 }
 
+
+//return <程序名字，使用到dll>
 std::map<std::wstring,std::vector<std::string>> fun( const TCHAR* pszPath )
 {
 	WIN32_FIND_DATA findFileData;
